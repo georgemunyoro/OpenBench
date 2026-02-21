2289efa8a5f58867ab3b4fab3a3cc0e71cc7e585
George Munyoro <george@munyoro.dev>
2026-02-22 01:15:39 +0200

    fix: improve error handling for PostgreSQL connection in entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,15 +3,19 @@ set -e
 
 echo "[entrypoint] Waiting for postgres..."
 until python -c "
-import os, psycopg2
-psycopg2.connect(
-    dbname=os.environ['POSTGRES_DB'],
-    user=os.environ['POSTGRES_USER'],
-    password=os.environ['POSTGRES_PASSWORD'],
-    host=os.environ.get('POSTGRES_HOST', 'db'),
-    port=os.environ.get('POSTGRES_PORT', '5432'),
-)
-" 2>/dev/null; do
+import os, sys, psycopg2
+try:
+    psycopg2.connect(
+        dbname=os.environ['POSTGRES_DB'],
+        user=os.environ['POSTGRES_USER'],
+        password=os.environ['POSTGRES_PASSWORD'],
+        host=os.environ.get('POSTGRES_HOST', 'db'),
+        port=os.environ.get('POSTGRES_PORT', '5432'),
+    )
+except Exception as e:
+    print(f'  {e}', file=sys.stderr)
+    sys.exit(1)
+"; do
   echo "[entrypoint] Postgres unavailable - sleeping 2s"
   sleep 2
 done
